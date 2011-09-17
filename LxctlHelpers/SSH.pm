02ac85cca9078c29790eff348a6fe63d21f5e8fe
Pavel Potapenkov <ppotapenkov@gmail.com>
2011-09-18 03:59:54 +0400

    Fixed a lot of migration bugs.
    Signed-off-by: Pavel Potapenkov <ppotapenkov@gmail.com>

--- LxctlHelpers/SSH.pm
+++ LxctlHelpers/SSH.pm
@@ -1,43 +1,41 @@
-package LxctlHelpers::SSH
+package LxctlHelpers::SSH;
 
 use strict;
+use warnings;
 use Net::SSH2;
+use Fcntl;
 
 my $ssh_ = Net::SSH2->new();
 
 sub connect {
-    my ($server, $user, $port) = @_;
+    my ($self, $server, $user, $port) = @_;
     $ssh_->connect($server);
     $ssh_->auth_publickey($user, '/root/.ssh/id_rsa.pub', '/root/.ssh/id_rsa');
     $ssh_->auth_ok() or die "Failed to authenticate at $user\@$server with public key.\n\n";
+
+    return $self;
 }
 
-sub exec {
-    my $cmd = shift;
-    my $ssh_ch = $ssh->channel();
+sub execute {
+    my ($self, $cmd) = @_;
+    my $ssh_ch = $ssh_->channel();
     $ssh_ch->exec($cmd);
 }
 
 sub get_file {
-    my ($from, $to) = @_;
-    my $sftp = $ssh_->sftp();
-    my $r_file = $sftp->open($from, O_RDONLY)
-        or die "Failed to open remote file $from.\n\n";
-    my $l_file = open($to, O_WRONLY|O_CREAT|O_TRUNC, 0600)
-        or die "Failed to open local file $to.\n\n";
+    my ($self, $from, $to) = @_;
 
-    print $l_file $_ while <$r_file>;
+    print "Copying local $from to remote $to...\n";
+
+    return $ssh_->scp_put($from, $to);
 }
 
 sub put_file {
-    my ($from, $to) = @_;
-    my $sftp = $ssh_->sftp();
-    my $r_file = $sftp->open($to, O_RDONLY)
-        or die "Failed to open remote file $to.\n\n";
-    my $l_file = open($from, O_WRONLY|O_CREAT|O_TRUNC, 0600)
-        or die "Failed to open local file $from.\n\n";
+    my ($self, $from, $to) = @_;
 
-    print $r_file $_ while <$l_file>;
+    print "Copying local $from to remote $to...\n";
+
+    return $ssh_->scp_put($from, $to);
 }
 
 1;
