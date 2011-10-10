d7dc886f34eb0e63f70cb30b9950416980d32fa4
Pavel Potapenkov <ppotapenkov@gmail.com>
2011-10-10 17:18:23 +0800

    Replaced Net::SSH2 with Net::OpenSSH. ssh-agent should work now.
    Signed-off-by: Pavel Potapenkov <ppotapenkov@gmail.com>

--- LxctlHelpers/SSH.pm
+++ LxctlHelpers/SSH.pm
@@ -2,50 +2,38 @@ package LxctlHelpers::SSH;
 
 use strict;
 use warnings;
-use Net::SSH2;
+use Net::OpenSSH;
 use Fcntl;
 
-my $ssh_ = Net::SSH2->new();
+my $ssh_;
 
 sub connect {
-    my ($self, $server, $user, $port) = @_;
-    $ssh_->connect($server);
-    $ssh_->auth_publickey($user, '/root/.ssh/id_rsa.pub', '/root/.ssh/id_rsa');
-    $ssh_->auth_ok() or die "Failed to authenticate at $user\@$server with public key.\n\n";
+	my ($self, $server, $user, $port) = @_;
+	$ssh_ = Net::OpenSSH->new("$user\@$server:$port");
+	$ssh_->error and die "Failed to connect to $user\@$host:$port: $ssh_->error.\n\n";
 
-    return $self;
+	return $self;
 }
 
 sub execute {
-    my ($self, $cmd) = @_;
-    my $ssh_ch = $ssh_->channel();
-    my $output, $len;
-    $ssh_ch->ext_data('merge');
-    $ssh_ch->exec($cmd);
-
-    $ssh_ch->close();
-    $ssh_ch->wait_closed();
-
-    $len = $ssh_ch->read($output,8192);
-    if ($len > 0) {
-        print "$output\n";
-    }
+	my ($self, $cmd) = @_;
+	$ssh_->system("$cmd") or die "Failed to execute $cmd: $ssh_->error.\n";
 }
 
 sub get_file {
-    my ($self, $from, $to) = @_;
+	my ($self, $from, $to) = @_;
 
-    print "Copying local $from to remote $to...\n";
+	print "Copying local $from to remote $to...\n";
 
-    return $ssh_->scp_put($from, $to);
+	return $ssh_->scp_put($from, $to);
 }
 
 sub put_file {
-    my ($self, $from, $to) = @_;
+	my ($self, $from, $to) = @_;
 
-    print "Copying local $from to remote $to...\n";
+	print "Copying local $from to remote $to...\n";
 
-    return $ssh_->scp_put($from, $to);
+	return $ssh_->scp_put($from, $to);
 }
 
 1;
