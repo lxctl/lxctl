620e9c1442311a0cadc58c37cb1558d3b3105204
Sergey Puzirev <spuzirev@meow.so>
2013-06-06 13:08:13 +0400

    * Fixed unmounting all mountpoints before stopping container * Addding stripping trailing slash in mountpoint --to option

--- src/lib/Lxctl/stop.pm
+++ src/lib/Lxctl/stop.pm
@@ -21,6 +21,14 @@ sub do
 	$options{'contname'} = shift
 		or die "Name the container please!\n\n";
 
+	eval {
+		$self->{'lxc'}->stop($options{'contname'});
+		print "It seems that \"$options{'contname'}\" is stopped now\n";
+	} or do {
+		print "$@";
+		die "Cannot stop $options{'contname'}!\n\n";
+	};
+
 	my $vm_option_ref;
 	my %vm_options;
 	$vm_option_ref = $config->load_file("$yaml_conf_dir/$options{'contname'}.yaml");
@@ -29,45 +37,40 @@ sub do
 	my @mount_points;
 	my $mount_result = `mount`;
 
-	if (defined $vm_options{'mountpoints'}) { 
-                my $mount_ref = $vm_options{'mountpoints'};
+	if (defined $vm_options{'mountpoints'}) {
+		my $mount_ref = $vm_options{'mountpoints'};
 
-                @mount_points = @$mount_ref;
-                if ($#mount_points == -1 ) {
-                        #print "No mount points specified!\n";
-                        last;
-                }
+		@mount_points = @$mount_ref;
+		if ($#mount_points == -1 ) {
+			#print "No mount points specified!\n";
+			last;
+		}
 
-                foreach my $mp_ref (@mount_points) {
-                        my %mp = %$mp_ref;
-                        my $cmd = "umount";
-                        my $to = quotemeta("$root_path/$options{'contname'}/rootfs$mp{'to'}");
+		foreach my $mp_ref (@mount_points) {
+			my %mp = %$mp_ref;
+			my $cmd = "umount";
+			my $to = quotemeta("$root_path/$options{'contname'}/rootfs$mp{'to'}");
 
-                        if ($mount_result =~ /on $to/) {
+			if ($mount_result =~ /on $to/) {
 				$cmd .= " $to";
 				system("$cmd");
 				if ( $? != 0 ) {
 					print "Can't umount $mp{'to'} !";
 				}
 			}
-                }
+		}
 	}
 
 	if (defined $vm_options{'rootfs_mp'}{'to'}) {
-		my $cmd = "umount $vm_options{'rootfs_mp'}{'to'}";
-		system("$cmd");
-		if ( $? != 0 ) {
-			print "Can't umount $vm_options{'rootfs_mp'}{'to'} !";
-		} 
+		my $to = quotemeta("$root_path/$options{'contname'}/rootfs");
+		if ($mount_result =~ /on $to/) {
+			my $cmd = "umount $vm_options{'rootfs_mp'}{'to'}";
+			system("$cmd");
+			if ( $? != 0 ) {
+				print "Can't umount $vm_options{'rootfs_mp'}{'to'} !";
+			}
+		}
 	}
-
-	eval {
-		$self->{'lxc'}->stop($options{'contname'});
-		print "It seems that \"$options{'contname'}\" is stopped now\n";
-	} or do {
-		print "$@";
-		die "Cannot stop $options{'contname'}!\n\n";
-	};
 
 	return;
 }
